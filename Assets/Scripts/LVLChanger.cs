469ef38109f8050714a1ca8adf221de0e799ddb1
bga <bga@rstgames.com>
2025-04-27 12:11:00 +0400

    fix (UI): Canvas resize

--- Assets/Scripts/LVLChanger.cs
+++ Assets/Scripts/LVLChanger.cs
@@ -1,9 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+[SerializeField]
+public enum Scenes
+{
+    MainMenu = 0,
+    Game = 1
+}
+
 public class LVLChanger : MonoBehaviour
 {
-    public void SwitchScene()
+    public static LVLChanger Instance => _instance;
+    private static LVLChanger _instance;
+
+    private void Awake()
     {
-        SceneManager.LoadScene(1);
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+    }
+    public void SwitchScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
     }
 }
--- Assets/Scripts/PausePanel.cs
+++ Assets/Scripts/PausePanel.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 using UnityEngine.Audio;
 public class PausePanel : MonoBehaviour
 {
+    private const string MusicVolumeKey = "MusicVolume";
+
     public AudioMixerGroup Mixer;
+
 
     //public AudioMixerSnapshot Normal;
     //public AudioMixerSnapshot InMenu;
-
+    //variableAudioSource.pitch = Random.Range(0.9f, 1.1f); 
     private void OnEnable()
     {
+        if (Mixer == null)
+        {
+            Mixer = GameObject.Find("MusicPlayer").GetComponent<AudioMixerGroup>();
+        }
         Time.timeScale = 0;
         //InMenu.TransitionTo(0,5f);
     }
@@ -18,16 +25,31 @@ public class PausePanel : MonoBehaviour
         Time.timeScale = 1;
     }
 
-    public void ChangeVolume(float volume) 
-    { 
-        Mixer.audioMixer.SetFloat("MusicVolume", Mathf.Lerp(-70, 5, volume));
+    public void ChangeVolume(float volume)
+    {
+        Mixer.audioMixer.SetFloat(MusicVolumeKey, Mathf.Lerp(-70, 5, volume));
         //PlayerPrefs.SetFloat("MusicVolume", volume);
-
-
     }
 
-    //variableAudioSource.pitch = Random.Range(0.9f, 1.1f);
+    public void SwichWisible()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
 
+    public void ResumeGame()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        LVLChanger.Instance.SwitchScene((int)Scenes.MainMenu);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 
 }
 
