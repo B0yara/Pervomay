c5dfbababb032f21c6e4dd6551fa0a4af56e7539
Timuery <adzartimur@gmail.com>
2025-05-02 10:23:09 +0300

    скриптик

---
+++ Assets/Scripts/shemeScripys/mainScripts.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class mainScripts : MonoBehaviour
+{
+
+}
+
+public class Moveble
+{
+    /// <summary>
+    /// Передвижение по X 
+    /// </summary>
+    /// <param name="t"></param>
+    /// <param name="sens"></param>
+    public void MoveX2D(Transform t, float sens)
+    {
+        Vector2 now = t.position;
+        now.x = now.x + sens;
+        t.position = now;
+    }
+    /// <summary>
+    /// Передвижение по Y
+    /// </summary>
+    /// <param name="t"></param>
+    /// <param name="sens"></param>
+    public void MoveY2D(Transform t, float sens)
+    {
+        Vector2 now = t.position;
+        now.y = now.y + sens;
+        t.position = now;
+    }
+    /// <summary>
+    /// Передвижение по 2D полю
+    /// </summary>
+    /// <param name="t"></param>
+    /// <param name="sens"></param>
+    public void Move2D(Transform t, Vector2 sens)
+    {
+        Vector3 nowPosition = t.position;
+        nowPosition.x = nowPosition.x + sens.x;
+        nowPosition.y = nowPosition.y + sens.y;
+        t.position = nowPosition;
+    }
+}
+public class sCamera
+{
+    public void OrthoZoom(Camera camera, float zoom)
+    {
+        camera.orthographicSize += zoom;
+    }
+}
+
+public class PlatformerController2D
+{
+    Rigidbody2D rb2d;
+    public void 
+}
